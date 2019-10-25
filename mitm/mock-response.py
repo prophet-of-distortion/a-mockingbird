5d73d97b2d075bce54d373bc225c638316a60349
Piercarlo Martino Peñero <pier@freelancer.com>
2019-10-25 22:36:24 +0800

    Refactor mock-response handler to accommodate for changes in the configuration.

--- mitm/mock-response.py
+++ mitm/mock-response.py
@@ -3,11 +3,9 @@ import json, os, re
 
 def response(flow: http.HTTPFlow) -> None:
     request_url = flow.request.url
-    with open(os.path.abspath(f"config/rewrite_mapping.json"), 'r') as f:
+    with open(os.path.abspath(f"config/mappings.json"), 'r') as f:
         rewrite_mapping = json.load(f)
-        for pattern, response_filename in rewrite_mapping:
+        for pattern, props in rewrite_mapping:
             if re.search(pattern, request_url):
-                response_file = open(os.path.abspath(f"response/{response_filename}.json"))
-                props = json.load(response_file)
                 flow.response = http.HTTPResponse.make(
                     props['status'], json.dumps(props['body']), props['header'])
