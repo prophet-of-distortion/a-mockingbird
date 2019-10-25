26422cc682611d2bd453c25c1bdf5bede57458c0
Piercarlo Martino Peñero <pier@freelancer.com>
2019-10-26 01:17:45 +0800

    Adds base retrieval using hooks.

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import ApiClient from './apiClient';
 
 const App: React.FC = () => {
-  const apiClient = new ApiClient();
+  const [mappings, setMappings] = useState([]);
+
+  useEffect(() => {
+    const apiClient = new ApiClient();
+    apiClient.getMappings().then((data) => {
+      setMappings(data);
+    });
+  }, []);
 
   return (
     <div>
       Mockingbird
+      <p>{JSON.stringify(mappings)}</p>
     </div>
   );
 }
