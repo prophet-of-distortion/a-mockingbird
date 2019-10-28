e0b5a7e778135bf08471a760df4b394a0cb1bc56
Piercarlo Martino Peñero <pier@freelancer.com>
2019-10-28 23:33:57 +0800

    Adds DELETE endpoint.

--- api/endpoints.py
+++ api/endpoints.py
@@ -10,9 +10,11 @@ def load_mapping_as_json():
         mappings = json.load(f)
     return mappings
 
+
 def save_mapping_as_json(data):
     with open(os.path.abspath(f"../config/mappings.json"), 'w') as f:
         json.dump(data, f, indent=2)
+
 
 @app.route('/mapping', methods=['POST'])
 def create_mapping():
@@ -21,13 +23,24 @@ def create_mapping():
     save_mapping_as_json(mappings)
     return jsonify(success=True, mappings=mappings)
 
+
+@app.route('/mapping/<int:index>', methods=['DELETE'])
+def delete_mapping(index):
+    if 0 > index or index >= len(load_mapping_as_json()):
+        return abort(404)
+    mappings = load_mapping_as_json()
+    del mappings[index]
+    save_mapping_as_json(mappings)
+    return jsonify(success=True, mappings=mappings)
+
+
 @app.route('/mappings', methods=['GET'])
 def get_all_mappings():
     return jsonify(load_mapping_as_json())
 
-@app.route('/mapping/<int:id>', methods=['PUT'])
-def update_mapping(id):
-    index = id;
+
+@app.route('/mapping/<int:index>', methods=['PUT'])
+def update_mapping(index):
     if 0 > index or index >= len(load_mapping_as_json()):
         return abort(404)
     mappings = load_mapping_as_json()
